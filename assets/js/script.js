49db897589ee1c14fb6a0f5677457a5c3e92688f
Blenda Orellana <blen.or90@gmail.com>
2021-06-16 16:10:38 -0500

    started working on js file, added timer and set up start button to start the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,32 @@
-/*Selects element by class
-var timeEl = document.querySelector(".time");
+var secondsLeft = 90;
+var timer;
+var timerCount;
+var startButton = document.querySelector(".start-button");
 
-// Selects element by id
-var mainEl = document.getElementById("main");
+let timeEl = document.querySelector (".timer");
 
-var secondsLeft = 10;*/
+function startGame() {
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderBlanks()
+    startTimer()
+  }
+
+function timer() {
+    let timerInterval = setInterval(function() {
+        timeEl.textContent = secondsLeft
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            timeEl.textContent = "Time's up!"
+        } else { 
+            timeEl.textContent = "Timer" + secondsLeft--;
+            timeEl.textContent = secondsLeft
+            
+        }
+    }, 1000);
+}
+
+startButton.addEventListener("click", startGame);
+
+timer();
