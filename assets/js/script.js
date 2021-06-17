ca03e3671ac74ead579a6bd99af70ae2f76f8091
Blenda Orellana <blen.or90@gmail.com>
2021-06-16 19:10:11 -0500

    styled box1 and answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,60 @@
+
+/*let start document.querySelector(".start");
+let quiz = document.querySelector(".quiz");
+let display = document.querySelector(".time");
+let question = document.querySelector(".questions");
+let optionA = document.querySelector(".A");
+let optionB = document.querySelector(".B");
+let optionC = document.querySelector(".C");*/
+
 var secondsLeft = 90;
 var timer;
-var timerCount;
-var startButton = document.querySelector(".start-button");
+var timerCount;/*
+var startButton = document.querySelector(".start-button");*/
 
 let timeEl = document.querySelector (".timer");
+/*
+let questions = {}
+    {
+        question : "How many planets are there in the universe?",
+        choiceA= "6",
+        choiceB= "8",
+        choiceC= "9",
+        choiceD="7"
+        correct= "B"
+    }
+
+    function renderQuestion(){
+        let q = questions[runningQuestion];
+        
+        question.innerHTML = "<p>"+ q.questions +"</p>";
+        optionA.innerHTML = q.optionA;
+        optionB.innerHTML = q.optionB;
+        optionC.innerHTML = q.optionC;
+        optionD.innerHTML = q.optionD;
+    }
+
+    start.addEventListener("click",startQuiz);
+
+// start quiz/*
+/*function startQuiz(){
+    start.style.display = "none";
+    runningQuestion = 0;
+    count = 0;
+    score = 0;
+    renderQuestion();
+    quiz.style.display = "block";
+    
+
+    
+    startTimer(30, display);
+}
 
 function startGame() {
     // Prevents start button from being clicked when round is in progress
     startButton.disabled = true;
-    renderBlanks()
     startTimer()
-  }
+  }*/
 
 function timer() {
     let timerInterval = setInterval(function() {
@@ -20,13 +64,13 @@ function timer() {
             clearInterval(timerInterval);
             timeEl.textContent = "Time's up!"
         } else { 
-            timeEl.textContent = "Timer" + secondsLeft--;
+            secondsLeft--;
             timeEl.textContent = secondsLeft
             
         }
     }, 1000);
 }
-
-startButton.addEventListener("click", startGame);
+/*
+startButton.addEventListener("click", startGame);*/
 
 timer();
