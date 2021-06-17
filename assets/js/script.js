ab9ef7850f5a8055c2d96a27b62015bcc71ebaa6
Blenda Orellana <blen.or90@gmail.com>
2021-06-17 09:57:43 -0500

    appended the questions and answers to the boxes in the html file, start buttons sets off timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 
-
 var secondsLeft = 90;
 var timer;
 var timerCount;
@@ -7,59 +6,61 @@ var questions = "";
 var score = 0;
 
 
-let timeEl = document.querySelector(".timer");/*
-let questionsEl = document.querySelector(".questions");
-let answersEl = document.querySelector(".answers");
-let startBtn = document.getElementById("#start-btn")
+let timeEl = document.querySelector(".timer");
 
 
+let questionsEl = document.querySelector(".question");
+let answersEl = document.querySelector(".answer");
+let startBtn = document.getElementById("start-btn")
+
+/*
 var allScores = [];
-var storedScores = JSON.parse(localStorage.getItem("userData"));
-
+var storedScores = JSON.parse(localStorage.getItem("userData"));*/
+var questionIndex = 0;
 var questions = [
     {
-        question= "How many planets are there our solar system?",
+        question: "How many planets are there in our Solar System?",
         options: ["6", "8", "9", "7"],
         answer: "8"
     },
     {
-        question= "Which planet has the most moons?",
+        question: "Which planet has the most moons?",
         options: ["Earth", "Mars", "Saturn", "Mercury"],
         answer: "Saturn"
 
     },
     {
-        question= "Which is the hottest planet in our solar system?",
+        question: "Which is the hottest planet in our Solar System?",
         options: ["Jupiter", "Mars", "Saturn", "Venus"],
         answer: "Venus"
     },
     {
-        question= "Which planet is the largest in our solar system?",
+        question: "Which planet is the largest in our Solar System?",
         options: ["Jupiter", "Mercury", "Earth", "Venus"],
         answer: "Jupiter"
     },
     {
-        question= "Which is the fifth larges planet in the Solar System?",
+        question: "Which is the fifth larges planet in our Solar System?",
         options: ["Jupiter", "Earth", "Saturn", "Venus"],
         answer: "Earth"
     },
 
 ]
 
-start - btn.addEventListener("click", startQuiz);
 
-function starQuiz() {
-    if (storedScores !== null) {
+
+function startQuiz() {
+    console.log("hey we are starting!")
+    /*if (storedScores !== null) {
         allScores = storedScores;
-    }
+    }*/
     timer()
-  
 
-    getQuestion() {
-
-    }
+    getQuestion() 
 }
-*/
+
+
+
 function timer() {
     let timerInterval = setInterval(function () {
         timeEl.textContent = secondsLeft
@@ -74,7 +75,26 @@ function timer() {
         }
     }, 1000);
 }
-/*
-startButton.addEventListener("click", startGame);*/
 
-timer();
+function getQuestion() { //empty and show the question and once you click answer it should go to the next question
+    // questionsEl.empty();
+    questionsEl.append(questions[questionIndex].question)
+
+    questions[questionIndex].options.forEach(function(singleAnswer){
+        console.log(singleAnswer)
+    // create one box only for the answers so they display in a list
+        answersEl.append(singleAnswer);
+        //Dinamically add buttons for the answers
+        //After an answer is clicked question index++ to move to the next question
+    })
+
+}
+//Append the questions and answers to the boxes 
+
+startBtn.addEventListener("click", startQuiz);
+
+
+/*
+timer();*/
+
+//data attributes if it tells you correct answer or wrong answer then it moves on 
