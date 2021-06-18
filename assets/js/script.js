27e7cec20f1abffda1873adf457be4d33f0e74e3
Blenda Orellana <blen.or90@gmail.com>
2021-06-17 22:00:35 -0500

    deleted buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,28 +21,28 @@ var questions = [
     {
         question: "How many planets are there in our Solar System?",
         options: ["6", "8", "9", "7"],
-        answer: "8"
+        correct: "8"
     },
     {
         question: "Which planet has the most moons?",
         options: ["Earth", "Mars", "Saturn", "Mercury"],
-        answer: "Saturn"
+        correct: "Saturn"
 
     },
     {
         question: "Which is the hottest planet in our Solar System?",
         options: ["Jupiter", "Mars", "Saturn", "Venus"],
-        answer: "Venus"
+        correct: "Venus"
     },
     {
         question: "Which planet is the largest in our Solar System?",
         options: ["Jupiter", "Mercury", "Earth", "Venus"],
-        answer: "Jupiter"
+        correct: "Jupiter"
     },
     {
         question: "Which is the fifth larges planet in our Solar System?",
         options: ["Jupiter", "Earth", "Saturn", "Venus"],
-        answer: "Earth"
+        correct: "Earth"
     },
 
 ]
@@ -78,44 +78,24 @@ function timer() {
     }, 1000);
 }
 
-function getQuestion() { //empty and show the question and once you click answer it should go to the next question
+function getQuestion() { 
+    
+    console.log//empty and show the question and once you click answer it should go to the next question
     // questionsEl.empty();
     questionsEl.append(questions[questionIndex].question)
 
     questions[questionIndex].options.forEach(function(singleAnswer){
-
         console.log(singleAnswer)
     // create one box only for the answers so they display in a list
         answersEl.append(singleAnswer);
-    {
-            // create new button for each choice
-            var choiceButton = document.createElement("button");
-            choiceButton.textContent = currentQuestion.choices[i];
-            
-            // display on the page
-            choicesEl.appendChild(choiceButton);
-          }
-          // attach click event listener to each choice
-          choicesEl.children[0].addEventListener("click", function(event){
-            questionClick(choicesEl.children[0]);
-          });
-          choicesEl.children[1].addEventListener("click", function(event){
-            questionClick(choicesEl.children[1]);
-          });
-          choicesEl.children[2].addEventListener("click", function(event){
-            questionClick(choicesEl.children[2]);
-          });
-          choicesEl.children[3].addEventListener("click", function(event){
-            questionClick(choicesEl.children[3]);
-          });
-        }
-    
 
-      
+        
 
         //Dinamically add buttons for the answers
         //After an answer is clicked question index++ to move to the next question
+    })
 
+}
 //Append the questions and answers to the boxes 
 
 startBtn.addEventListener("click", startQuiz);
